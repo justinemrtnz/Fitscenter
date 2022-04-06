22781133d66c9cbe2a8e5552c1ab63663488f0d9
unknown <justinemrtnz16@gmail.com>
2022-04-06 10:05:21 +0800

    initial commit

--- app.py
+++ app.py
@@ -5,27 +5,47 @@ import os
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER']="static\images"
+app.config['UPLOAD_FOLDER'] = "static\images"
 currentdirectory = os.path.dirname(os.path.abspath(__file__))
+
+
 def get_db_connection():
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
     return conn
+
+
 @app.route('/')
 def hello_world():  # put application's code here
     return render_template('home.html')
+
 
 @app.route('/login', methods=('GET', 'POST'))
 def home():
     return render_template('home.html')
 
+
 @app.route('/agriculturist', methods=('GET', 'POST'))
 def agriculturist():
     return render_template('agriculturist.html')
 
+
 @app.route('/farmers', methods=('GET', 'POST'))
 def farmers():
     return render_template('farmers.html')
+
+
+@app.route('/view')
+def view():
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close()
+    return render_template('post.html', posts=posts)
+
+@app.route('/pdf')
+def pdf():
+    return render_template('materials.html')
+
 
 @app.route('/upnews', methods=('GET', 'POST'))
 def upnews():
@@ -33,10 +53,12 @@ def upnews():
 
         caption = request.form['caption']
         pic = request.files['upload_img']
-        if pic.filename!="":
-            filepath=os.path.join(app.config['UPLOAD_FOLDER'],pic.filename)
+        if pic.filename != "":
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], pic.filename)
+
             pic.save(filepath)
             conn = get_db_connection()
+
             conn.execute('INSERT INTO posts (caption, content) VALUES (?, ?)',
                          (caption, pic.filename))
             conn.commit()
